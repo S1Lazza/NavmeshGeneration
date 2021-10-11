7453743138da966e7140538ee53eedc3da3f6b52
S1Lazza <lazzaroni.stefao93@gmail.com>
2021-10-11 21:03:23 +0200

    [FIX] Wrong include file which prevented compilation of the project

--- Source/Navmesh_Generation/NavMesh/CustomNavigationData.cpp
+++ Source/Navmesh_Generation/NavMesh/CustomNavigationData.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "CustomNavigationData.h"
 #include "NavMeshGenerator.h"
 #include "NavmeshController.h"
-#include "CustomNavigationData.h"
 
 FPathFindingResult ACustomNavigationData::FindPath(const FNavAgentProperties& AgentProperties, const FPathFindingQuery& Query)
 {
